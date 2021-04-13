6c38a2ed85f6dc79cfc3ab5fe28049140113fe83
2F4S7 <nightsta1kerhf@gmail.com>
2021-04-13 06:06:53 +0300

    color-switch

--- js/color-switch.js
+++ js/color-switch.js
@@ -10,3 +10,27 @@ const colors = [
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
+
+const btn = document.querySelectorAll('button');
+const start = btn[0];
+const stop = btn[1];
+
+function createBackgraundColor() {
+  document.body.style.backgroundColor =
+    colors[randomIntegerFromInterval(0, colors.length - 1)];
+}
+
+let intervalId = 0;
+
+function activationStart() {
+  intervalId = setInterval(createBackgraundColor, 1000);
+  start.disabled = 'true';
+}
+
+function clickStop() {
+  clearInterval(intervalId);
+  start.disabled = '';
+}
+
+start.addEventListener('click', activationStart);
+stop.addEventListener('click', clickStop);
